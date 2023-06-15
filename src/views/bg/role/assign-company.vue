2b00fed873522c69bf8db2ddd128434eca5e2dff
yueting.tian <werner1901@163.com>
2023-06-15 18:47:30 +0800

    拆开公司分配

---
+++ src/views/bg/role/assign-company.vue
@@ -0,0 +1,113 @@
+<template>
+  <resourcedialog
+    :title="title"
+    :dialogType="dialogType"
+    :visible.sync="companydialogVisible"
+    @assignCompany="assignCompany"
+  >
+    <el-table
+      ref="companyTable"
+      v-loading="listLoading"
+      :data="companyData"
+      selectable
+      highlight-current-row
+      height="350"
+      style="width: 100%"
+    >
+      <el-table-column type="selection" />
+      <el-table-column label="公司编号" align="center" prop="code" />
+      <el-table-column label="公司名称" align="center" prop="name" />
+    </el-table>
+  </resourcedialog>
+</template>
+<script>
+import resourcedialog from './resource-dialog.vue'
+import { commonQuery as companyQuery} from '@/api/org/company'
+import { modifyRoleCompany } from '@/api/right/role'
+export default {
+  name: "assigncompany",
+  components:{resourcedialog},
+  props: {
+    roleVo:{ type: undefined, required:true,default:[]},
+  },
+  data() {
+    return {
+      title:'公司分配',
+      dialogType:'company',
+      companydialogVisible:false,
+      listLoading:false,
+      roleIdList:[],
+      companyData:[],
+      roleCompanyDTO:{}
+    };
+  },
+  methods: {
+    /**
+     * 获取所有公司信息
+     */
+     getCompanySearch() {
+      this.companydialogVisible = true
+      this.listLoading = true
+      companyQuery({}).then(res => {
+        this.companyData = res.data
+        this.fillCompanyInfo(roleVo)
+      })
+      .catch(() => {
+        this.$message.error('公司信息获取失败')
+      })
+      .finally(() => {
+        this.listLoading = false
+      })
+    },
+    /**
+     * 公司信息预填入
+     * @param roleVo 所选行的信息
+     */
+    fillCompanyInfo(roleVo) {
+      if (roleVo.length === 1) {
+        // 获取角色对应的授权公司列表
+        queryRoleCompany(roleVo[0].id).then(res => {
+          const roleAuthCompanyIds = res.authCompanyIds
+          if (roleAuthCompanyIds) {
+            this.$nextTick(() => {
+              // 遍历table的数据并进行判断
+              this.companyData.forEach(company => {
+                // 如果某一行的company_id在该角色对应的授权公司列表中，将其勾选
+                if (roleAuthCompanyIds.indexOf(company.id) > -1) {
+                  this.$refs.companyTable.toggleRowSelection(company, true)
+                }
+              })
+            })
+          }
+        })
+        .catch(() => {
+          this.$message.error("角色所属公司信息获取失败")
+        })
+      }
+    },
+    /**
+     * 公司分配
+     */
+     assignCompany() {
+      this.roleCompanyDTO.roleIds = this.roleVo.map(role => role.id)
+      this.roleCompanyDTO.authCompanyIds = this.$refs.companyTable.selection.map(company => company.id)
+      modifyRoleCompany(this.roleCompanyDTO)
+      .then(res => {
+        if (res) {
+          this.companydialogVisible = false
+          this.$message.success("角色所属公司分配成功")
+        } else {
+          this.$message.error("角色所属公司信息分配失败")
+        }
+      })
+      .catch(() => {
+        this.$message.error("角色所属公司信息分配失败")
+      })
+      .finally(() => {
+        this.roleCompanyDTO.roleIds = []
+        this.roleCompanyDTO.authCompanyIds = []
+      })
+    }
+  }
+};
+</script>
--- src/views/bg/role/index.vue
+++ src/views/bg/role/index.vue
@@ -242,7 +242,7 @@
       >
     </assignresource>
     <!-- 公司分配的弹框 -->
-    <resourcedialog
+    <!-- <resourcedialog
       :title="'公司分配'"
       dialogType='company'
       :visible.sync="companydialogVisible"
@@ -261,7 +261,13 @@
         <el-table-column label="公司编号" align="center" prop="code" />
         <el-table-column label="公司名称" align="center" prop="name" />
       </el-table>
-    </resourcedialog>
+    </resourcedialog> -->
+    <assigncompany
+      :roleVo="roleVo"
+      ref="assignrolecompany"
+      @fetchData="fetchData"
+      >
+    </assigncompany>
   </div>
 </template>
 
@@ -289,11 +295,12 @@ import resourcedialog from './resource-dialog.vue'
 import tablepagination from './pagination.vue'
 import assignresource from './assign-resource.vue'
 import assignuser from './assign-user.vue'
+import assigncompany from './assign-company.vue'
 
 
 export default {
   name: 'Role',
-  components:{AddAndUpdateDialog,searchbox,resourcedialog,tablepagination,assignresource,assignuser},
+  components:{AddAndUpdateDialog,searchbox,resourcedialog,tablepagination,assignresource,assignuser,assigncompany},
   data: function() {
     return {
       addTitle: '增加角色',
@@ -1004,73 +1011,62 @@ export default {
      * 获取所有公司信息
      */
     getCompanyAll() {
-      const roleVo = this.$refs.roleTable.selection
-      if (roleVo == null || roleVo.length === 0) {
+      this.roleVo = this.$refs.roleTable.selection
+      if (this.roleVo == null || this.roleVo.length === 0) {
         this.$message.error('至少选择一个角色进行操作')
         return
       }
-      this.companydialogVisible = true
-      this.listLoading = true
-      companyQuery({}).then(res => {
-        this.companyData = res.data
-        this.fillCompanyInfo(roleVo)
-      })
-      .catch(() => {
-        this.$message.error('公司信息获取失败')
-      })
-      .finally(() => {
-        this.listLoading = false
-      })
+      this.$refs.assignrolecompany.getCompanySearch();
     },
     /**
      * 公司信息预填入
      * @param roleVo 所选行的信息
      */
-    fillCompanyInfo(roleVo) {
-      if (roleVo.length === 1) {
-        // 获取角色对应的授权公司列表
-        queryRoleCompany(roleVo[0].id).then(res => {
-          const roleAuthCompanyIds = res.authCompanyIds
-          if (roleAuthCompanyIds) {
-            this.$nextTick(() => {
-              // 遍历table的数据并进行判断
-              this.companyData.forEach(company => {
-                // 如果某一行的company_id在该角色对应的授权公司列表中，将其勾选
-                if (roleAuthCompanyIds.indexOf(company.id) > -1) {
-                  this.$refs.companyTable.toggleRowSelection(company, true)
-                }
-              })
-            })
-          }
-        })
-        .catch(() => {
-          this.$message.error("角色所属公司信息获取失败")
-        })
-      }
-    },
+    // fillCompanyInfo(roleVo) {
+    //   if (roleVo.length === 1) {
+    //     // 获取角色对应的授权公司列表
+    //     queryRoleCompany(roleVo[0].id).then(res => {
+    //       const roleAuthCompanyIds = res.authCompanyIds
+    //       if (roleAuthCompanyIds) {
+    //         this.$nextTick(() => {
+    //           // 遍历table的数据并进行判断
+    //           this.companyData.forEach(company => {
+    //             // 如果某一行的company_id在该角色对应的授权公司列表中，将其勾选
+    //             if (roleAuthCompanyIds.indexOf(company.id) > -1) {
+    //               this.$refs.companyTable.toggleRowSelection(company, true)
+    //             }
+    //           })
+    //         })
+    //       }
+    //     })
+    //     .catch(() => {
+    //       this.$message.error("角色所属公司信息获取失败")
+    //     })
+    //   }
+    // },
     /**
      * 公司分配
      */
-    assignCompany() {
-      this.roleCompanyDTO.roleIds = this.$refs.roleTable.selection.map(role => role.id)
-      this.roleCompanyDTO.authCompanyIds = this.$refs.companyTable.selection.map(company => company.id)
-      modifyRoleCompany(this.roleCompanyDTO)
-      .then(res => {
-        if (res) {
-          this.companydialogVisible = false
-          this.$message.success("角色所属公司分配成功")
-        } else {
-          this.$message.error("角色所属公司信息分配失败")
-        }
-      })
-      .catch(() => {
-        this.$message.error("角色所属公司信息分配失败")
-      })
-      .finally(() => {
-        this.roleCompanyDTO.roleIds = []
-        this.roleCompanyDTO.authCompanyIds = []
-      })
-    }
+    // assignCompany() {
+    //   this.roleCompanyDTO.roleIds = this.$refs.roleTable.selection.map(role => role.id)
+    //   this.roleCompanyDTO.authCompanyIds = this.$refs.companyTable.selection.map(company => company.id)
+    //   modifyRoleCompany(this.roleCompanyDTO)
+    //   .then(res => {
+    //     if (res) {
+    //       this.companydialogVisible = false
+    //       this.$message.success("角色所属公司分配成功")
+    //     } else {
+    //       this.$message.error("角色所属公司信息分配失败")
+    //     }
+    //   })
+    //   .catch(() => {
+    //     this.$message.error("角色所属公司信息分配失败")
+    //   })
+    //   .finally(() => {
+    //     this.roleCompanyDTO.roleIds = []
+    //     this.roleCompanyDTO.authCompanyIds = []
+    //   })
+    // }
   }
 }
 </script>
